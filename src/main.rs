80ecc2757e4f7e46dbd2a2a205788d4a066fdc57
Shounak <shounsach@gmail.com>
2023-08-03 17:24:38 +0530

    not going to mess up err handling now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:3459")
+        .await
+        .unwrap_or_else(|err| panic!("Problem binding to socket: {err:?}"));
+
+    let (stream, _socketaddr) = listener
+        .accept()
+        .await
+        .unwrap_or_else(|err| panic!("Problem reading stream: {err:?}"));
 }
