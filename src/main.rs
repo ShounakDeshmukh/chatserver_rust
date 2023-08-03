291cae74cf5c027387d2a3197754f9249df17937
Shounak <shounsach@gmail.com>
2023-08-03 18:34:15 +0530

    multi threading implemented multiple clients supported

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,32 @@ async fn main() {
     let listener = TcpListener::bind("localhost:3459")
         .await
         .unwrap_or_else(|err| panic!("Problem binding to socket: {err:?}"));
-
-    let (mut stream, _socketaddr) = listener
-        .accept()
-        .await
-        .unwrap_or_else(|err| panic!("Problem establising connection: {err:?}"));
-
-    let (read, mut writer) = stream.split();
-    let mut line = String::new();
-    let mut reader = BufReader::new(read);
-
     loop {
-        let bytes_read = reader
-            .read_line(&mut line)
+        let (mut stream, _socketaddr) = listener
+            .accept()
             .await
-            .unwrap_or_else(|err| panic!("Problem reading stream: {err:?}"));
+            .unwrap_or_else(|err| panic!("Problem establising connection: {err:?}"));
 
-        if bytes_read == 0 {
-            break;
-        }
-        writer
-            .write_all(&line.as_bytes())
-            .await
-            .unwrap_or_else(|err| panic!("Problem writing to stream: {err:?}"));
-        line.clear()
+        tokio::spawn(async move {
+            let (read, mut writer) = stream.split();
+            let mut line = String::new();
+            let mut reader = BufReader::new(read);
+
+            loop {
+                let bytes_read = reader
+                    .read_line(&mut line)
+                    .await
+                    .unwrap_or_else(|err| panic!("Problem reading stream: {err:?}"));
+
+                if bytes_read == 0 {
+                    break;
+                }
+                writer
+                    .write_all(&line.as_bytes())
+                    .await
+                    .unwrap_or_else(|err| panic!("Problem writing to stream: {err:?}"));
+                line.clear()
+            }
+        });
     }
 }
